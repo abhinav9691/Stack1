bd46877ef519b6595bae83916a249884de64f89b
abhinav9691 <81373991+abhinav9691@users.noreply.github.com>
2023-06-03 09:11:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using StackTest;
+
+StackEx ex = new StackEx();
+ex.Push(20);
+ex.Push(40);
+ex.Push(50);
+ex.Push(60);
+
+ex.Display();
---
+++ StackEx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackTest
+{
+    internal class StackEx
+    {
+        Node top;
+
+        public StackEx()
+        {
+            top = null;
+        }
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = top;
+            top = newNode;
+        }
+
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.WriteLine("| " +temp.Data + " |");
+                    Console.WriteLine("---------");
+                    temp = temp.Next;
+                }
+            }
+        }
+    }
+}
